c7e51822f64cae8573fcfa58494f5bb4842cbd27
Artem <ghosttalion@gmail.com>
2023-09-19 20:43:33 +0300

    first commit

---
+++ TestTask/cell/PersonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PersonTableViewCell.swift
+//  TestTask
+//
+//  Created by Artem Krasko on 05.09.2023.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TestTask/name.swift
@@ -0,0 +1,8 @@
+//
+//  name.swift
+//  TestTask
+//
+//  Created by Artem Krasko on 05.09.2023.
+//
+
+import Foundation
